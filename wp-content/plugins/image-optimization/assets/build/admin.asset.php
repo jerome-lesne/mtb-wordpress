d87e10adbe851c5aed3a344529c6d55b36c724b1
Jerome <jeromelesne@gmail.com>
2024-02-21 17:10:44 +0100

    update

--- wp-content/plugins/image-optimization/assets/build/admin.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '7f417a976b1665c063fc');
--- wp-content/plugins/image-optimization/assets/build/control.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-a11y', 'wp-api-fetch', 'wp-escape-html', 'wp-i18n'), 'version' => '8cf6baba91294e6f8a46');
